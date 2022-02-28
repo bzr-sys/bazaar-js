a91aad8b99b9f60212c763249d377311886e7da3
Paddy O'Hanlon <paddy@paddyohanlon.com>
2022-02-28 18:31:44 +0100

    Forgot to build

--- dist/rethinkid-js-sdk.esm.js
+++ dist/rethinkid-js-sdk.esm.js
@@ -44,6 +44,19 @@ async function pkceChallengeFromVerifier(codeVerifier) {
 
 /**
  * The primary class of the RethinkID JS SDK to help you more easily build web apps with RethinkID.
+ *
+ * @example
+ * ```
+ * import { RethinkID } from "@mostlytyped/rethinkid-js-sdk";
+ *
+ * const config = {
+ *   appId: process.env.VUE_APP_APP_ID,
+ *   signUpRedirectUri: process.env.VUE_APP_SIGN_UP_REDIRECT_URI,
+ *   logInRedirectUri: process.env.VUE_APP_LOG_IN_REDIRECT_URI,
+ * };
+ *
+ * export const rid = new RethinkID(config);
+ * ```
  */
 class RethinkID {
     #signUpBaseUri = "http://localhost:3000/sign-up";
--- dist/types/index.d.ts
+++ dist/types/index.d.ts
@@ -1,6 +1,19 @@
 import { Options, TokenDecoded, IdTokenDecoded, Permission } from "./types";
 /**
  * The primary class of the RethinkID JS SDK to help you more easily build web apps with RethinkID.
+ *
+ * @example
+ * ```
+ * import { RethinkID } from "@mostlytyped/rethinkid-js-sdk";
+ *
+ * const config = {
+ *   appId: process.env.VUE_APP_APP_ID,
+ *   signUpRedirectUri: process.env.VUE_APP_SIGN_UP_REDIRECT_URI,
+ *   logInRedirectUri: process.env.VUE_APP_LOG_IN_REDIRECT_URI,
+ * };
+ *
+ * export const rid = new RethinkID(config);
+ * ```
  */
 export declare class RethinkID {
     #private;
