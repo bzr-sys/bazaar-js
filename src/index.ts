c0f534afdaf10579d1305636a935c08cda43b12d
Paddy O'Hanlon <paddy@paddyohanlon.com>
2022-03-02 18:34:54 +0100

    Add user info utility method
    For getting only user ID and scoped claims from an ID token.

--- src/index.ts
+++ src/index.ts
@@ -243,6 +243,31 @@ export class RethinkID {
     }
   }
 
+  /**
+   * A utility function to get user info, i.e. user ID and the scope-based claims of an
+   * authenticated user's ID token.
+   */
+  userInfo(): null | { userId: string; email: string; name: string } {
+    const idToken = localStorage.getItem(idTokenKeyName);
+
+    if (idToken) {
+      try {
+        const idTokenDecoded: IdTokenDecoded = jwt_decode(idToken);
+
+        return {
+          userId: idTokenDecoded.sub || "",
+          email: idTokenDecoded.email || "", // emailVerified is redundant because log in requires verification
+          name: idTokenDecoded.name || "",
+        };
+      } catch (error) {
+        // Error decoding ID token, assume token is invalid and remove
+        localStorage.removeItem(idTokenKeyName);
+      }
+    }
+
+    return null;
+  }
+
   // Data API
 
   /**
