676318b5ce5df12999c1741e5d0633c152a75bf9
Paddy <ohanlon.paddy@gmail.com>
2022-04-28 11:03:24 +0200

    Add window.crypto.subtle support check, remove debugging comments

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -12,13 +12,14 @@ export function generateRandomString(): string {
  * @param input A random string
  */
 function sha256(input: string): Promise<ArrayBuffer> {
-  console.log("sha256:");
   const encoder = new TextEncoder();
-  console.log("- encoder", encoder);
   const data = encoder.encode(input);
-  console.log("- data", data);
-  console.log("- window.crypto", window.crypto);
-  console.log("- window.crypto.subtle", window.crypto.subtle);
+
+  if (!window.crypto.subtle) {
+    console.error(
+      "The RethinkID JS SDK works with https or localhost. Possibly you're trying to use it with http. Reason: window.crypto.subtle requires https in most browsers.",
+    );
+  }
   return window.crypto.subtle.digest("SHA-256", data);
 }
 
@@ -42,8 +43,6 @@ function base64UrlEncode(arrayBuffer: ArrayBuffer): string {
  * @param codeVerifier A random string
  */
 export async function pkceChallengeFromVerifier(codeVerifier: string): Promise<string> {
-  console.log("pkceChallengeFromVerifier:");
   const hashed = await sha256(codeVerifier);
-  console.log("- hashed", hashed);
   return base64UrlEncode(hashed);
 }
