472dc23866cb2b0009bbc3c7cf82df5e186a1e4d
Paddy <ohanlon.paddy@gmail.com>
2023-11-13 17:18:08 +0100

    Add json import assertion
    Required above node v17.5.0

--- rollup.config.js
+++ rollup.config.js
@@ -3,7 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import typescript from "rollup-plugin-typescript2";
-import pkg from "./package.json";
+import pkg from "./package.json" assert { type: "json" };
 
 /**
  * @type {import('rollup').RollupOptions}
