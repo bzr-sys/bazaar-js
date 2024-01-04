17111e59f199c2e1419f87ba6bdc660518912dee
Paddy <ohanlon.paddy@gmail.com>
2024-01-04 13:08:10 +0100

    Replace node var in bundled IIFE

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,7 @@ import resolve from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
 import babel from "@rollup/plugin-babel";
 import typescript from "rollup-plugin-typescript2";
+import replace from '@rollup/plugin-replace';
 import pkg from "./package.json" assert { type: "json" };
 
 /**
@@ -48,6 +49,10 @@ const config = [
         exclude: ["node_modules/**"],
         babelHelpers: "bundled",
         external: [/@babel\/runtime/],
+      }),
+      replace({
+        'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV || 'production'),
+        preventAssignment: true,
       }),
     ],
   },
