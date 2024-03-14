6383951e91e5214f099fdb52ee5c2fada9dab372
Sascha Trifunovic <sascha.trifunovic@gmail.com>
2024-03-14 10:18:32 +0100

    Update default URI

--- src/constants/index.ts
+++ src/constants/index.ts
@@ -1,7 +1,7 @@
 /**
  * The URI of the current Bazaar deployment
  */
-export const bazaarUri = process.env.NODE_ENV == "development" ? "http://localhost:3377" : "https://id.rethinkdb.cloud";
+export const bazaarUri = process.env.NODE_ENV == "development" ? "http://localhost:3377" : "https://api.bzr.dev";
 
 /**
  * The URL path for sharing links
